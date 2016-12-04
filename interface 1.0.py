dbffc82a200942c3c5f235ddbf7289cea7d9cf30
nathanlederman <nathan.lederman1996@gmail.com>
2016-12-04 16:59:20 -0200

    interface 1.2

--- interface 1.0.py
+++ interface 1.0.py
@@ -1,11 +1,8 @@
 from tkinter import *
-class Interface:
-    def __init__(self):
-         
-        self.janela = Tk()
-        self.janela["bg"] = "black"
-        self.janela.title("CompareRide")
-        self.janela.geometry("400x1200+300+300")
+janela1 = Tk()
+janela1["bg"] = "black"
+janela1.title("CompareRide")
+janela1.geometry("400x1200+300+300")
 
 def bt_click():
     print("bt_click")
@@ -13,25 +10,37 @@ def bt_click():
         
         num1 = int(ed1.get())
         num2 = int(ed2.get())    
-        lb1["text"]  = num1 + num2
-    else:
-        lb1["text"] = "valores informados invalidos"
-
-def entrada(self):
+        lb3["text"]  = num1 + num2
         
-    self.ed1 = Entry(self.janela)
-    self.ed1.place(x= 100, y = 100)
-    self.ed2= Entry(self.janela)
-    self.ed2.place(x=100,y=130)
-
-    self.bt = Button(self.janela, width = 20, text = "Comparar", command=bt_click)
-    self.bt.place(x= 100, y = 150)
-
-    self.lb1 = Label(self.janela, text = "Label1")
-    self.lb1.place(x=100, y = 200)
+    elif lb3["text"] == "valores informados invalidos":
+        pass
 
 
+    
+ed1 = Entry(janela1)
+ed1.place(x= 100, y = 100)
+lb1 = Label(janela1, width = 10, text = "Tempo",fg="black",bg="gold")
+lb1.place(x = 0, y =100)
+ed2= Entry(janela1)
+ed2.place(x=100,y=130)
+lb2 = Label(janela1, width = 10, text = "Distancia",fg="black",bg="gold")
+lb2.place(x = 0, y =130)
+lb3 = Label(janela1, width = 10,text = "Comparar",fg="black",bg="gold")
+lb3.place(x = 100, y =180)
+    
+bt = Button(janela1, width = 20, text = "Comparar", command=bt_click)
+bt.place(x= 100, y = 150)
 
+janela1.mainloop()
+
+  
+janela=Tk()
+janela["bg"] = "black"
+janela.title("CompareRide")
+janela.geometry("400x1200+300+300")
 janela.mainloop()
 
+if bt_click == True:
+    janela1.destroy()
+    janela.mainloop
 
